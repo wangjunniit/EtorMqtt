39183bda566e9ff2c632f2e25a24cd0c702d7cd4
王军 <wangjun.aspx@qq.com>
2020-09-30 09:58:03 +0800

    日志配置与启动脚本调整

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
-exec java \
+#! /bin/bash
+
+javahome="/opt/jdk-15"
+
+
+exec $javahome/bin/java \
 --add-opens=java.base/sun.net.dns=ALL-UNNAMED \
 --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
 --add-opens=java.base/java.nio=ALL-UNNAMED \
@@ -8,6 +12,8 @@ exec java \
 -Dio.netty.tryReflectionSetAccessible=true \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseZGC \
+-Xlog:safepoint,classhisto*=trace,age*,gc*=info:file=Logs/gc-%t.log:time,tid,tags:filecount=5,filesize=50m \
+-XX:ReservedCodeCacheSize=128m -XX:InitialCodeCacheSize=128m \
 -Xms512M \
 -Xmx2G \
 -jar $(dirname "$0")/etormqtt.jar
