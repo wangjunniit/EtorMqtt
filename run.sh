29f9a02fc2dc0314094ae17f6bdb2c80e9d16f96
王军 <wangjun.aspx@qq.com>
2020-12-18 14:46:25 +0800

    更新包

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 javahome="/opt/jdk-15"
-
+mkdir -p $(dirname "$0")/Logs/GC
 
 exec $javahome/bin/java \
 --add-opens=java.base/sun.net.dns=ALL-UNNAMED \
@@ -12,7 +12,7 @@ exec $javahome/bin/java \
 -Dio.netty.tryReflectionSetAccessible=true \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseZGC \
--Xlog:safepoint,classhisto*=trace,age*,gc*=info:file=Logs/gc-%t.log:time,tid,tags:filecount=5,filesize=50m \
+-Xlog:safepoint,classhisto*=trace,age*,gc*=info:file=Logs/GC/gc-%t.log:time,tid,tags:filecount=5,filesize=50m \
 -XX:ReservedCodeCacheSize=128m -XX:InitialCodeCacheSize=128m \
 -Xms512M \
 -Xmx2G \
